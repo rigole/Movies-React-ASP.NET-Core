51bd47b61eaaefd7b7918d7588c369405b8126cb
rigole <foplacide@gmail.com>
2023-03-05 15:02:35 +0100

    folder for movie component, its style, its model interfce and loading data statically

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Movies-React-ASP.NET-Core
+
+This repository is to build a movie list app with React in the frontend and ASP.NET Core and C# in the backend
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import IndividualMovie from './movies/IndividualMovie';
 import './App.css';
+import { movieDTO } from './movies/movies.model';
 
 function App() {
+
+  const testMovie: movieDTO = {
+    id: 1,
+    title: "Madame Monsieur",
+    poster: 'https://bocir-prod-bucket.s3.amazonaws.com/medias/W3HrhWazhO/image/MadameMonsieur1657351653514.jpg'
+  }
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <IndividualMovie {...testMovie}/>
+    </>
   );
 }
 
