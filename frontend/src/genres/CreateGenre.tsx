228dc901b29c226380038a1ad598bdf9cfb47113
rigole <foplacide@gmail.com>
2023-03-11 07:59:40 +0100

    creating the editGenre and createGenre component

---
+++ frontend/src/genres/CreateGenre.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function CreateGenre(){
+
+    return(
+        <>
+          <h3>Create Genre</h3> 
+      
+        </>
+    )
+}
+
+export default CreateGenre
