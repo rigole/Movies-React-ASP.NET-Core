0ad4e18fa460f80fb5bb9950b2f20b57284d0771
rigole <foplacide@gmail.com>
2023-03-20 12:33:08 +0100

    creating a model for genres

---
+++ frontend/src/genres/GenreForm.tsx
@@ -0,0 +1,34 @@
+import { Formik, FormikHelpers } from "formik";
+import { Form, Link } from "react-router-dom";
+import TextField from "../forms/TextField";
+import Button from "../utils/Button";
+import * as Yup from 'yup';
+import { genreCreationDTO } from '../genres/genres.model'
+
+function GenreForm(props: genreFormProps){
+
+    return (
+        <Formik initialValues={props.model} 
+          onSubmit={props.onSubmit}
+          validationSchema={Yup.object({
+            name: Yup.string().required('This field is required').firstLetterUppercase()  
+          })}
+          >
+            {(formikProps) => (
+              <Form>
+                <TextField field="name" displayName="Name"/>
+                <Button disabled={formikProps.isSubmitting} type="submit">Save Changes</Button>
+                <Link className="btn btn-secondary" to="/genres">Cancel</Link>
+             </Form>
+            )}
+            
+          </Formik>
+    )
+}
+
+export default GenreForm
+
+interface genreFormProps {
+    model: genreCreationDTO;
+    onSubmit(values: genreCreationDTO, action: FormikHelpers<genreCreationDTO>): void;
+}
