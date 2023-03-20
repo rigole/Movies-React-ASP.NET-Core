7f4956c9145e1d10df163597b2006694048c518c
rigole <foplacide@gmail.com>
2023-03-20 16:03:47 +0100

    creating a model for genres 2

--- frontend/src/genres/EditGenre.tsx
+++ frontend/src/genres/EditGenre.tsx
@@ -1,11 +1,18 @@
 import  { useParams } from "react-router-dom";
+import GenreForm from "./GenreForm";
 
 function EditGenre(){
     const {id} = useParams();
     return (
         <>
             <h3>Edit Genre</h3>
-            The is the id number {id}
+            <GenreForm model={{name: 'Action'}} 
+          onSubmit={ async value => {
+            await new Promise(r => setTimeout(r, 1));
+            console.log(id)            
+            console.log(value);
+          }}
+          />
         </>
     )
 }
