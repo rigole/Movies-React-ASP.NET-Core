853080ea3dc9dc30710b1608be3336a8d529d611
rigole <foplacide@gmail.com>
2023-03-24 13:25:14 +0100

    Creating date component to work with date field

--- frontend/src/actors/ActorForm.tsx
+++ frontend/src/actors/ActorForm.tsx
@@ -1,5 +1,6 @@
 import { Formik, Form, FormikHelpers } from "formik";
 import TextField from "../forms/TextField";
+import DateFields from "../forms/DateField";
 import Button from "../utils/Button";
 import { Link } from "react-router-dom";
 import { actorCreationDTO } from "./actors.model";
@@ -13,12 +14,14 @@ function ActorForm(props: actorFormProps){
         <Formik initialValues={props.model} 
         onSubmit={props.onSubmit}
         validationSchema={Yup.object({
-            name: Yup.string().required("This field is required").firstLetterUppercase()
+            name: Yup.string().required("This field is required").firstLetterUppercase(),
+            dateOfBirth: Yup.date().nullable().required('This field is required')
         })}
         >
             {(formikProps) => (
                 <Form>
                     <TextField displayName="Name" field="name"/>
+                    <DateFields displayName="Date of Birth" field="dateOfBirth" />
                     <Button disabled={formikProps.isSubmitting} type="submit">Save Changes</Button>
                     <Link to="/actors" className="btn btn-secondary">Cancel</Link>
                 </Form>
---
+++ frontend/src/forms/DateField.tsx
@@ -0,0 +1,35 @@
+import { useFormikContext } from "formik";
+
+function DateFields(props: dateFieldProps) {
+    
+    const {values, validateForm, touched, errors} = useFormikContext<any>();
+    return (
+        <div>
+            <label htmlFor={props.field}>{props.displayName}</label>
+            <input type="date" className="form-control" 
+                id={props.field}
+                name={props.field}
+                defaultValue={values[props.field]?.toLocalDateString('en-US')}
+                onChange={e => {
+                    const date = new Date(e.currentTarget.value + 'T00:00:00');
+                    values[props.field] = date;
+                    validateForm();
+                }}
+            />
+            {touched[props.field] && errors[props.field] ? 
+            <div className="text-danger">{errors[props.field]?.toString()}</div>:null}
+        </div>
+    )
+}
+
+
+
+
+interface dateFieldProps{
+    field: string;
+    displayName: string;
+
+}
+
+
+export default DateFields
